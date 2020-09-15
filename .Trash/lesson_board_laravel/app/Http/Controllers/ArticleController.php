b81f636e0eb64716fcc0e7d4d2793e395f4a501e
“shun1012” <s.koyama1012@gmail.com>
2020-09-15 14:39:32 +0900

    lesson_board削除

---
+++ .Trash/lesson_board_laravel/app/Http/Controllers/ArticleController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Article;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $message = 'Welcome my BBS';
+        return view('index', ['message' => $message]);
+    }
+}
