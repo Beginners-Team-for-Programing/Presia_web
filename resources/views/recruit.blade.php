1b1393928ec8d6b082dbd8252efcff1b5cf57240
“shun1012” <s.koyama1012@gmail.com>
2020-10-14 09:50:53 +0900

    絵？追加

--- resources/views/recruit.blade.php
+++ resources/views/recruit.blade.php
@@ -1,10 +1,23 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
+<html lang="ja" dir="ltr">
     <head>
         <meta charset="utf-8">
         <title>presia/recruit</title>
     </head>
     <body>
+        <pre>
 
+
+            8888888bo.
+            888'  '88P
+            888    888
+            888.  .888                                            o888
+            8888888Pd'    oooo d8b     .od88bo.      .d8888b.
+            888           "888""8P    8'      '8     88'          oooo     .od88bo.d8b
+            888            888        P'88888P'       Y'888b.     "888     8'     '88
+            888            888        bY.                  88      888     8       88
+            888            888         Y888888P'     .d88888P      888     Y'88888P"Y8b.
+
+        </pre>
     </body>
 </html>
