6215cd865bbe84d8ec80c17519b1c2b9d589e1e5
hitomi-uchida <hi_chun_zyanken@icloud.com>
2020-10-19 23:31:26 +0900

    css、js作成

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,9 +5,14 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>TOP</title>
+  <link rel="stylesheet" href="{{ asset('/css/index.css') }}">
+  <scriptsrc="{{ asset('/js/index.js') }}">
+    </script>
+
 </head>
 
 <body>
+  <img alt="" src="{{ asset('/img/presia.png') }}">
   <h1>PRESIA</h1>
   <ul>
     <li><a href=''>Top</a></li>
