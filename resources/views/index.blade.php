b1dc98958e1da867bd29ed56b49fe67fc39bb030
Junpei <dpsknu25@gmail.com>
2020-11-30 21:54:40 +0900

    20201130

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -217,29 +217,27 @@
           </tr>
         </table>
       </div>
-
-      <div id="wrapper6">
-        <div class="access">
-          <h2>ACCESS</h2>
-          <p>所在地：154-0001 東京都世田谷区池尻2-33-14 クレストビル５階</p>
-          <p>最寄駅：田園都市線 池尻大橋駅　徒歩３分</p>
-
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3242.1338338467995!2d139.67986371519984!3d35.64907413934231!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6018f4a8947ef97b%3A0xa4b90507c017fd52!2z44CSMTU0LTAwMDEg5p2x5Lqs6YO95LiW55Sw6LC35Yy65rGg5bC777yS5LiB55uu77yT77yT4oiS77yR77yU!5e0!3m2!1sja!2sjp!4v1604329490150!5m2!1sja!2sjp"
-            width="400" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false"
-            tabindex="0"></iframe><br>
-          <a
-            href="https://www.google.com/maps/place/%E6%A0%AA%E5%BC%8F%E4%BC%9A%E7%A4%BE%E3%83%97%E3%83%AC%E3%82%B8%E3%82%A2/@35.6490831,139.6798687,17z/data=!3m1!4b1!4m5!3m4!1s0x60188c7f31915555:0x225fea8d935519a6!8m2!3d35.6490831!4d139.6820574">
-            詳しい地図をgooglemapで見る
-          </a>
-        </div>
-      </div>
+    </div>
+  </div>
+  <div id="wrapper6">
+    <div class="access">
+      <h2>ACCESS</h2>
+      <p>所在地：154-0001 東京都世田谷区池尻2-33-14 クレストビル５階</p>
+      <p>最寄駅：田園都市線 池尻大橋駅　徒歩３分</p>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3242.1338338467995!2d139.67986371519984!3d35.64907413934231!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6018f4a8947ef97b%3A0xa4b90507c017fd52!2z44CSMTU0LTAwMDEg5p2x5Lqs6YO95LiW55Sw6LC35Yy65rGg5bC777yS5LiB55uu77yT77yT4oiS77yR77yU!5e0!3m2!1sja!2sjp!4v1604329490150!5m2!1sja!2sjp"
+        width="400" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false"
+        tabindex="0"></iframe><br>
+      <a
+        href="https://www.google.com/maps/place/%E6%A0%AA%E5%BC%8F%E4%BC%9A%E7%A4%BE%E3%83%97%E3%83%AC%E3%82%B8%E3%82%A2/@35.6490831,139.6798687,17z/data=!3m1!4b1!4m5!3m4!1s0x60188c7f31915555:0x225fea8d935519a6!8m2!3d35.6490831!4d139.6820574">
+        詳しい地図をgooglemapで見る
+      </a>
     </div>
   </div>
   <div id="wrapper7">
     <div class="contact">
       <h2>CONTACT</h2>
-      <form action="/confirm" method="POST">
+      <form class="form" action="/confirm" method="POST">
         @csrf
         <div class="form-group">
           @error('name')
@@ -271,7 +269,7 @@
             <p>ERROR {{$message}}</p>
           </div>
           @enderror
-          <textarea name="contents" value="Message" placeholder="Message"> {{old('contents')}}</textarea>
+          <textarea name="contents" value="{{old('contents')}}" placeholder="Message"></textarea>
         </div>
         <div id="send">
           <input type="submit" value="Send">
