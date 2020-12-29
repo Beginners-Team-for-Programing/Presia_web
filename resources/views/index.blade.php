7e66f4e7e0d40bae86a96bc8e4364f4b5b91410d
hitomi-uchida <hi_chun_zyanken@icloud.com>
2020-12-29 21:48:38 +0900

    レスポンシブ２

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,6 +7,9 @@
 
   <title>株式会社 Presia|関わるすべての人を幸せに</title>
 
+  <!-- header nav -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="js/header-nav.js"></script>
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
     integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
@@ -30,9 +33,8 @@
 </head>
 
 <body>
+
   <!--
-
-
         8888888bo.
         888'  '88P
         888    888
@@ -42,35 +44,53 @@
         888            888        P'88888P'       Y'888b.     "888     8'     '88
         888            888        bY.                  88      888     8       88
         888            888         Y888888P'     .d88888P      888     Y'88888P"Y8b.
-
         私たちと一緒に、事業やサービスを作りませんか？　  https://www.00000000000000000000000000
-
     -->
+
   <div class="back-button">
     <p class="pagetop"><a href="#wrap">▲</a></p>
   </div>
-  <div id="header">
-    <img id="logo" alt="presiarogo" src="{{ asset('images/presia.png') }}">
-    <nav class="navbar navbar-expand-md navbar-light">
-      <div class="nav">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item"><a href='#wrapper'>TOP</a></li>
-            <li class="nav-item"><a href='#wrapper2'>ABOUT</a></li>
-            <li class="nav-item"><a href='#wrapper3'>SERVICE</a></li>
-            <li class="nav-item"><a href='#wrapper4'>RECRUIT</a></li>
-            <li class="nav-item"><a href='#wrapper5'>COMPANY</a></li>
-            <li class="nav-item"><a href="#wrapper6">ACCESS</a></li>
-            <li class="nav-item"><a href='#wrapper7'>CONTACT</a></li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+  <div id="res-nav">
+    <div class="logo">
+      <a href="/"><img id="logo" alt="presiarogo" src="{{ asset('images/presia.png') }}"></a>
+    </div>
+    <div class="navigation">
+      <ul>
+        <li><a href='#wrapper'>TOP</a></li>
+        <li><a href='#wrapper2'>ABOUT</a></li>
+        <li><a href='#wrapper3'>SERVICE</a></li>
+        <li><a href='#wrapper4'>RECRUIT</a></li>
+        <li><a href='#wrapper5'>PROFILE</a></li>
+        <li><a href="#wrapper6">ACCESS</a></li>
+        <li><a href='#wrapper7'>CONTACT</a></li>
+      </ul>
+    </div>
+    <div class="res-sns">
+      <a href='https://ja-jp.facebook.com/presia.co.jp'><img src="images/fb.png"><a>
+    </div>
   </div>
+
+  <div id="header">
+    <div class="logo">
+      <a href="/"><img id="logo" alt="presiarogo" src="{{ asset('images/presia.png') }}"></a>
+    </div>
+    <div class="nav">
+      <ul>
+        <li><a href='#wrapper'>TOP</a></li>
+        <li><a href='#wrapper2'>ABOUT</a></li>
+        <li><a href='#wrapper3'>SERVICE</a></li>
+        <li><a href='#wrapper4'>RECRUIT</a></li>
+        <li><a href='#wrapper5'>PROFILE</a></li>
+        <li><a href="#wrapper6">ACCESS</a></li>
+        <li><a href='#wrapper7'>CONTACT</a></li>
+      </ul>
+    </div>
+    <div class="sns">
+      <a href='https://ja-jp.facebook.com/presia.co.jp'><img src="images/fb.png"><a>
+    </div>
+  </div>
+  <img id="open-button" src="images/menu-open.png">
+  　<img id="close-button" src="images/menu-close.png">
 
   <div class="mainvisual">
     <video autoplay loop>
@@ -82,160 +102,148 @@
     </div>
   </div>
 
-  <div id="wrapper2">
-    <div class="vision">
-      <h2>ABOUT US</h2>
-      <p>私たち株式会社プレジアは創業当初より、事業を通じて<br>
-        『関わる全ての人を幸せにすること』＝「世の中を、より良くすること」を目指しています。<br>
-        クライアント、社員、その家族、全ての人の「幸せ」を実現するために、<br>
-        既存の形に捉われない、過去の成功に満足しない、<br>
-        この精神で常に現状に疑問を持ち、日々挑戦していきます。</p>
-    </div>
+  <div class="vision">
+    <h2>ABOUT US</h2>
+    <p>私たち株式会社プレジアは創業当初より、事業を通じて<br>
+      『関わる全ての人を幸せにすること』＝「世の中を、より良くすること」を目指しています。<br>
+      クライアント、社員、その家族、全ての人の「幸せ」を実現するために、<br>
+      既存の形に捉われない、過去の成功に満足しない、<br>
+      この精神で常に現状に疑問を持ち、日々挑戦していきます。</p>
   </div>
-  <div id="wrapper3">
-    <div class="main">
-      <div class="service">
-        <h2>SERVICE</h2>
+
+  <div class="service">
+    <h2>SERVICE</h2>
+    <div class="service-1">
+      <div class="service-contents order2">
+        <h3>飲食店経営</h3>
+        <p>
+          都心の港区を中心に飲食店経営を行っております。
+          日本中の強豪がひしめき合う街、六本木。新規参入の難易度は非常に高く、
+          また、参入しても１年と持たないお店が大半を占める中で、
+          私たちプレジアは、１０年間直営店を運営してきております。
+          世の中に必要とされる飲食事業を創造し続けております。
+        </p>
       </div>
-      <div class="service-1">
-        <div class="service-contents order2">
-          <h3>飲食店経営</h3>
-          <!-- <div class="text right order2"> -->
-          <p>
-            都心の港区を中心に飲食店経営を行っております。
-            日本中の強豪がひしめき合う街、六本木。新規参入の難易度は非常に高く、
-            また、参入しても１年と持たないお店が大半を占める中で、
-            私たちプレジアは、１０年間直営店を運営してきております。
-            世の中に必要とされる飲食事業を創造し続けております。
-          </p>
-          <!-- </div> -->
-        </div>
-        <div class="service-img order1">
-          <img src="{{ asset('/images/restaurant.jpg') }}" alt="restaurant-management">
-        </div>
+      <div class="service-img order1">
+        <img src="{{ asset('/images/restaurant.jpg') }}" alt="restaurant-management">
       </div>
-      <div class="service-2">
-        <div class="service-img order1">
-          <img src="{{ asset('/images/consul.png') }}" alt="consultant">
-        </div>
-        <div class="service-contents order2">
-          <h3>飲食コンサル</h3>
-          <p>
-            夢を追い求める飲食店経営者を支援したい、という想いから
-            長年の飲食店経営や事業経営によって蓄積させたノウハウを元に、
-            店舗管理の仕組み作りからお客様サポート、人材の採用や仕入れに至るまでを一貫して提供しています。
-          </p>
-        </div>
+    </div>
+    <div class="service-2">
+      <div class="service-img order1">
+        <img src="{{ asset('/images/consul.png') }}" alt="consultant">
       </div>
-      <div class="service-3">
-        <div class="service-contents order2">
-          <h3>Web制作</h3>
-          <p>
-            『ITを通して、関わる全ての人を幸せにする』
-            をテーマに、高品質なホームページ制作と丁寧なアフターサポートによって
-            お客様のWEB戦略の一員として共にお仕事させていただいております。
-            はじめてホームページを制作する方も、ホームページを作り変えたい方も、
-            是非ご要望をお聞かせください。
-          <p>
-        </div>
-        <div class="service-img order1">
-          <img src="{{ asset('/images/web.jpg') }}" alt="WebProduction">
-        </div>
+      <div class="service-contents order2">
+        <h3>飲食コンサル</h3>
+        <p>
+          夢を追い求める飲食店経営者を支援したい、という想いから
+          長年の飲食店経営や事業経営によって蓄積させたノウハウを元に、
+          店舗管理の仕組み作りからお客様サポート、人材の採用や仕入れに至るまでを一貫して提供しています。
+        </p>
+      </div>
+    </div>
+    <div class="service-3">
+      <div class="service-contents order2">
+        <h3>Web制作</h3>
+        <p>
+          『ITを通して、関わる全ての人を幸せにする』
+          をテーマに、高品質なホームページ制作と丁寧なアフターサポートによって
+          お客様のWEB戦略の一員として共にお仕事させていただいております。
+          はじめてホームページを制作する方も、ホームページを作り変えたい方も、
+          是非ご要望をお聞かせください。
+        <p>
+      </div>
+      <div class="service-img order1">
+        <img src="{{ asset('/images/web.jpg') }}" alt="WebProduction">
       </div>
     </div>
   </div>
 
 
-
-  <div id="wrapper4">
-    <div class="recruit">
-      <h2>RECRUIT</h2>
-      <div class="recruit-img">
-        <img src="{{ asset('/images/recruit.jpg') }}" alt="recruit_2">
-      </div>
-      <div class="content">
-        <p>私たちプレジアは、時間の制約や場所の制約等、様々な制約を超えて、今まで合わさることのなかった「個性」の組み合わせによる未来の創出を描いています。
-          今まで培ってきた「個性」は、必ず活かせます。
-          あなたの「個性」を使って、たくさんの幸せの創出を一緒にしませんか？
-          プレジアでは経験者から未経験者、業務委託と、幅広く採用を行っています。
-          小さい会社ならではの幅広い権限を生かして、思い描く理想の働き方を実現しましょう。
-          弊社のメンバーと楽しく会社を作っていけるメンバーを募集します。</p>
-      </div>
+  <div class="recruit">
+    <h2>RECRUIT</h2>
+    <div class="recruit-img">
+      <img src="{{ asset('/images/recruit.jpg') }}" alt="recruit_2">
+    </div>
+    <div class="content">
+      <p class="cont">私たちプレジアは、時間の制約や場所の制約等、様々な制約を超えて、今まで合わさることのなかった「個性」の組み合わせによる未来の創出を描いています。
+        今まで培ってきた「個性」は、必ず活かせます。
+        あなたの「個性」を使って、たくさんの幸せの創出を一緒にしませんか？
+        プレジアでは経験者から未経験者、業務委託と、幅広く採用を行っています。
+        小さい会社ならではの幅広い権限を生かして、思い描く理想の働き方を実現しましょう。
+        弊社のメンバーと楽しく会社を作っていけるメンバーを募集します。</p>
+      <p class="res-cont">プレジアでは経験者から未経験者、業務委託と、幅広く採用を行っています。
+        小さい会社ならではの幅広い権限を生かして、思い描く理想の働き方を実現しましょう。</p>
     </div>
   </div>
-  <div id="wrapper5">
-    <div class="main-2">
-      <div class="campany">
-        <div class="cam">
-          <h2>PROFILE</h2>
-        </div>
-        <table class="campany-box">　
-          <tr>
-            <th>会社名</th>
-            <td>株式会社プレジア</td>
-          </tr>
-          <tr>
-            <th>代表取締役</th>
-            <td>関澤光太</td>
-          </tr>
-          <tr>
-            <th>資本金</th>
-            <td>￥2,000,000</td>
-          </tr>
-          <tr>
-            <th>所在地</th>
-            <td>〒154-0001 東京都世田谷区池尻2-33-14 クレストビル５階</td>
-          </tr>
-          <tr>
-            <th>従業員数</th>
-            <td>１０名（パート・アルバイト含む）</td>
-          </tr>
-          <tr>
-            <th>電話番号</th>
-            <td>03-6413-8117</td>
-          </tr>
-          <tr>
-            <th>受付時間</th>
-            <td>平日10:00-18:00 （土日は休業）</td>
-          </tr>
-        </table>
-      </div>
-      <div class="history">
-        <div class="his">
-          <h2>HISTORY</h2>
-        </div>
-        <table class="history-box">
-          <tr>
-            <th>2013年10月</th>
-            <td>Roppongi BAR DEN オープン</td>
-          </tr>
-          <tr>
-            <th>2013年10月</th>
-            <td>BAR　GLOSSオープン</td>
-          </tr>
-          <tr>
-            <th>2013年10月</th>
-            <td>BAR HEVENオープン</td>
-          </tr>
-          <tr>
-            <th>2014年03月</th>
-            <td>株式会社プレジア設立</td>
-          </tr>
-          <tr>
-            <th>2015年10月</th>
-            <td>東京都港区に本社移転</td>
-          </tr>
-          <tr>
-            <th>2018年10月</th>
-            <td>事業拡大に伴い、本店を東京都目黒区に移転</td>
-          </tr>
-          <tr>
-            <th>2020年10月</th>
-            <td>事業拡大に伴い、東京都渋谷区に本社移転</td>
-          </tr>
-        </table>
-      </div>
+  <div class="campany">
+    <div class="cam">
+      <h2>PROFILE</h2>
     </div>
+    <table class="campany-box">　
+      <tr>
+        <th>会社名</th>
+        <td>株式会社プレジア</td>
+      </tr>
+      <tr>
+        <th>代表取締役</th>
+        <td>関澤光太</td>
+      </tr>
+      <tr>
+        <th>資本金</th>
+        <td>￥2,000,000</td>
+      </tr>
+      <tr>
+        <th>所在地</th>
+        <td>〒154-0001 東京都世田谷区池尻2-33-14 クレストビル５階</td>
+      </tr>
+      <tr>
+        <th>従業員数</th>
+        <td>１０名（パート・アルバイト含む）</td>
+      </tr>
+      <tr>
+        <th>電話番号</th>
+        <td>03-6413-8117</td>
+      </tr>
+      <tr>
+        <th>受付時間</th>
+        <td>平日10:00-18:00 （土日は休業）</td>
+      </tr>
+    </table>
+  </div>
+  <div class="history">
+    <div class="his">
+      <h2>HISTORY</h2>
+    </div>
+    <table class="history-box">
+      <tr>
+        <th>2013年10月</th>
+        <td>Roppongi BAR DEN オープン</td>
+      </tr>
+      <tr>
+        <th>2013年10月</th>
+        <td>BAR　GLOSSオープン</td>
+      </tr>
+      <tr>
+        <th>2013年10月</th>
+        <td>BAR HEVENオープン</td>
+      </tr>
+      <tr>
+        <th>2014年03月</th>
+        <td>株式会社プレジア設立</td>
+      </tr>
+      <tr>
+        <th>2015年10月</th>
+        <td>東京都港区に本社移転</td>
+      </tr>
+      <tr>
+        <th>2018年10月</th>
+        <td>事業拡大に伴い、本店を東京都目黒区に移転</td>
+      </tr>
+      <tr>
+        <th>2020年10月</th>
+        <td>事業拡大に伴い、東京都渋谷区に本社移転</td>
+      </tr>
+    </table>
   </div>
   <div id="wrapper6">
     <div class="access">
@@ -326,9 +334,6 @@
 
 
   <div class="footer">
-    <div class="footer-sns">
-      <a href='https://ja-jp.facebook.com/presia.co.jp'><img src="images/facebook.png"><a>
-    </div>
     <div class="footer-text">
       <small>©︎ 2020 PRESIA Co.,Ltd.</small><br>
       <a href="/privacy/">プライバシーポリシー</a>
